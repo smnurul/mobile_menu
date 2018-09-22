58aa6578822e9b7abd004cdaf13bbe2af395b414
SM NURUL ISLAM <43316959+smnurul@users.noreply.github.com>
2018-09-23 00:14:16 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+	jQuery('.navbar').meanmenu();
+	
+    
+    var typed = new Typed('.typed_test', {
+  strings: [ "hello world",
+            
+            "welcome to my website",
+            "I'm Freelancer Nurul Islam",
+           "I'm a Full Stack web Developer",
+            "What I do beside?",
+            " Web Application Development",
+            " Wordpress Theme Development ",
+            "eCommerce Website"
+           ],
+  typeSpeed: 70,
+    loop:true
+});
+});
